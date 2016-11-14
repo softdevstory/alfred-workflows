d592452152ac12bd45bf07746c27c6471e700e56
softdevstory <developer@softdevstory.net>
2016-11-14 21:52:05 +0900

    version 0.1

---
+++ raywenderlich/README.md
@@ -0,0 +1,16 @@
+# Alfred workflow for getting raywenderlich.com feed([Download](raywenderlich.alfredworkflow))
+
+Display the recent ariticles from [raywenderlich.com](https://www.raywenderlich.com)
+
+![Screenshot](screenshot.png)
+
+# Usage
+
+```
+ ray
+```
+
+# Used Libraies
+
+* [feedparser](https://github.com/kurtmckee/feedparser)
+* [alfred-workflow](https://github.com/deanishe/alfred-workflow)
---
+++ raywenderlich/main.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+#
+# developer@softdevstory.net, 2016
+
+import sys
+import feedparser
+from workflow import Workflow3, ICON_WEB
+
+def get_feed_data():
+    return feedparser.parse('https://raywenderlich.com/feed')
+
+def main(wf):
+
+    feed = wf.cached_data('feed', get_feed_data, max_age=60)
+    for item in feed.entries:
+        wf.add_item(item.title,
+                    item.link,
+                    quicklookurl=item.link,
+                    icon=ICON_WEB,
+                    valid=True,
+                    arg=item.link)
+                    
+    wf.send_feedback()
+    
+
+if __name__ == '__main__':
+    wf = Workflow3()        
+    sys.exit(wf.run(main))
